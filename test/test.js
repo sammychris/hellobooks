dfd81c2da06d8dc68bcc6e9f9f0e51f20d853820
Samuel Christopher <Ebusameric@gmail.com>
2018-07-23 19:17:08 +0100

    modified

--- test/test.js
+++ test/test.js
@@ -6,19 +6,19 @@ chai.use(chaiHttp);
 
 
 describe('Route', () => {
-  
-  it('should return a 200 response', function(done){
+
+  it('should return a 200 response', function (done) {
     chai.request(app)
-    	.get('/api/')
-    	.end((err, res) => { res.should.have.status(200)});
-    	done();
+      .get('/api/')
+      .end((err, res) => { res.should.have.status(200) });
+    done();
   });
 
   it('should return a 200 response', (done) => {
     chai.request(app)
-    	.get('/api/books')
-    	.end((err, res) => { res.should.have.status(200)});
-    	done();
+      .get('/api/books')
+      .end((err, res) => { res.should.have.status(200) });
+    done();
   });
 
 });
