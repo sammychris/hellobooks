00b4458c1785029a9077dc14f45a872154f52bd7
Samuel Christopher <Ebusameric@gmail.com>
2018-07-23 17:46:34 +0100

    update to es6 syntax

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,23 @@
-import signUpCrl from '../controllers/user';
-import signInCrl from '../controllers/user';
-import userCrl from '../controllers/user';
-import bookCrl from '../controllers/books';
+import user from '../controllers/user';
+import book from '../controllers/books';
 import auth from '../MIDDLEWARE/authorize';
+
+const { createUser, signInUser } = user;
+const { addBook, list, modify, getBorrowedBooks, borrowABook } = book;
+
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
 
-  app.post('/api/user/signup', signUpCrl.createUser);
-  app.post('/api/user/signin', signInCrl.signInUser);
-  app.post('/api/book', bookCrl.addBook);
-  app.get('/api/books', bookCrl.list);
-  app.put('/api/books/:id', bookCrl.modify);
-  app.get('/api/users/:userId/books', userCrl.getBorrowedBooks);
-  app.post('/api/users/:userId/books', userCrl.borrowABook);
+  app.post('/api/user/signup', createUser);
+  app.post('/api/user/signin', signInUser);
+  app.post('/api/book', addBook);
+  app.get('/api/books', list);
+  app.put('/api/books/:id', modify);
+  app.get('/api/users/:userId/books', getBorrowedBooks);
+  app.post('/api/users/:userId/books', borrowABook);
 
   // app.get('/api/books/:id', bookCrl.list); //not working yet;
 };
