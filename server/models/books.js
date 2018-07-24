cf282a40aad6161a07f7e01bf578de78dc9d8162
Samuel Christopher <Ebusameric@gmail.com>
2018-07-24 20:23:43 +0100

    add validation for book model

--- server/models/books.js
+++ server/models/books.js
@@ -1,11 +1,47 @@
 
 export default (sequelize, DataTypes) => {
   const books = sequelize.define('book', {
-    Tittle: { type: DataTypes.STRING, allowNull: false, },
-    Author: { type: DataTypes.STRING, allowNull: false, },
-    Category: { type: DataTypes.STRING, allowNull: false, },
-    Quantity: { type: DataTypes.INTEGER, allowNull: false, },
-    Description: { type: DataTypes.STRING, allowNull: false, },
+    Tittle: { 
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate:
+            { 
+                notEmpty: true,
+                len: [3,30] 
+            } 
+      },
+    Author: { 
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate:
+            { 
+                notEmpty: true,
+                len: [3,30] 
+            } 
+      },
+    Category: { 
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate:
+            { 
+                notEmpty: true,
+                len: [3,30] 
+            } 
+      },
+    Quantity: { 
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 1
+      },
+    Description: { 
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate:
+            { 
+                notEmpty: true,
+                len: [10,500] 
+            } 
+      },
   }, {
       classMethods: {
         associate: (models) => { // eslint-disable-line
