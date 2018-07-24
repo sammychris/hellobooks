74802066a32c6c059f317d522429243030afd505
Samuel Christopher <Ebusameric@gmail.com>
2018-07-24 20:15:55 +0100

    add validation on user models

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,41 @@
 
 export default (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
-    email: { type: DataTypes.STRING, allowNull: false, },
-    username: { type: DataTypes.STRING, allowNull: false, },
-    password: { type: DataTypes.STRING, allowNull: false, },
-    membership: { type: DataTypes.STRING, allowNull: false, },
+    email: { 
+        type: DataTypes.STRING,
+         allowNull: false,
+         validate:
+            { 
+                isEmail: true,
+                notEmpty: true,
+            } 
+    },
+    username: { 
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate:
+            { 
+                notEmpty: true,
+                len: [3,30] 
+            } 
+      },
+    password: { 
+        type: DataTypes.STRING,
+         allowNull: false,
+         validate:{ 
+                  notEmpty: true,
+                  len: [8,40] 
+                } 
+         },
+    membership: { 
+        type: DataTypes.STRING,
+          allowNull: false,
+          validate:
+            { 
+                notEmpty: true,
+                len: [3,30] 
+            } 
+     },
   }, {
     classMethods: {
         associate: (models) => { // eslint-disable-line
