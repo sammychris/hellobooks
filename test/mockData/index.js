1e5d4256c3c24ff6b58cefe4e39c79a73583e7da
Samuel Christopher <Ebusameric@gmail.com>
2018-07-26 01:01:18 +0100

    add mock data for test

---
+++ test/mockData/index.js
@@ -0,0 +1,50 @@
+
+
+
+export const newUser = {
+				email: 'jacksone@gmail.com',
+				username: 'sammatins',
+				password: '22384938999',
+				membership: 'Gold'
+		};
+
+export const fakeUserEmail = {
+				email: 'jacksonegmail.com',
+				username: 'sammatinddss',
+				password: '22384938ss999',
+				membership: 'Gold'
+		};
+
+export const oldUser = {
+				username: 'sammatins',
+				password: '22384938999',
+		};
+
+export const adminUser = {
+				username: 'sammatins',
+				password: '22384938999',
+				admin: true
+		};
+
+export const fakeAdmin = {
+				username:'dlkksjldjlsj0',
+				password: 'dldidkshdls',
+				admin: true
+		};
+
+export const fakeUser = {
+				username: 'esammenjjjsshek',
+				password: 'sldjsdhjdkhehek',
+		}
+
+export const book = {
+                    Tittle: 'The Book of John',
+                    Author: 'John the beloved',
+                    Category: 'Bible',
+                    Quantity: 3,
+                    Description: 'This book is God\'s word, and most powerful gift too man.'
+                };
+
+export const borrowBookById = { 
+						bookId : 5
+					};
